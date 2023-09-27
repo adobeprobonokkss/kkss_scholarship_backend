832e0eee72e810952c3bbb4f2478b9216e261d78
Shravan Shetty <shettyshravan24@gmail.com>
2023-09-27 16:51:34 +0530

    refactoring

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,8 @@ import config from "config";
 import { logger } from "./utils/logger";
 import routes from "./routes";
 const functions = require("firebase-functions");
-var RateLimit = require("express-rate-limit");
 
 const app = express();
-var limiter = RateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -24,7 +17,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 routes(app);
-app.use(limiter);
 
 logger.info("app is listening");
 
